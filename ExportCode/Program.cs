be06fe6fcf8518ffe54f6a1b45f6b633c2d6cb5d
MaddoScientisto <maddo.science@gmail.com>
2016-11-19 19:52:29 +0100

    Exportcode

---
+++ ExportCode/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ExportCode
+{
+    class Program
+    {
+        private string workingFolder;
+
+        static void Main(string[] args)
+        {
+            ExportCode e = new ExportCode();
+            e.Program(args);
+            
+
+        }
+
+        
+    }
+}
